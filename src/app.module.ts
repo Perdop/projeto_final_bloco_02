ad61e4f11cab28170908b25308350f5e791046fb
Pedro <operdop@proton.me>
2025-12-17 09:26:42 -0300

    feat db, entities e config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DevService } from './data/services/dev.service';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: DevService,
+      imports: [ConfigModule],
+    }),
+    CategoriaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createConnection } from 'mysql2/promise';
+import { readFileSync } from 'fs';
+import { ValidationPipe } from '@nestjs/common';
+
+async function ensureDatabase() {
+  const connection = await createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+  });
+
+  const sql = readFileSync('src/sql/create-database.sql').toString();
+  await connection.query(sql);
+}
 
 async function bootstrap() {
+  await ensureDatabase();
+
   const app = await NestFactory.create(AppModule);
+
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
